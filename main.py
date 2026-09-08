b21b3c9a3ac006d54db2d56634ae53b22fde4943
Sourish <sourish19z@gmail.com>
2026-09-08 17:29:55 +0530

    completed

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from utils import parse_log_lines
+from pathlib import Path
+
+from utils import is_after_timestamp, parse_log_lines, validate_date_time
 
 
 def parse_log_file(filepath:str):
     """Return list of parsed log entries (tuples)"""
-    entries:list[tuple[str,str,str,str]] = []
+    path = Path(filepath)
+
+    if path.exists() is False:
+      return None
+
+    entries:list[tuple[str,str,str]] = []
 
     with open(filepath,"r") as file:
       for line in file:
@@ -14,40 +21,72 @@ def parse_log_file(filepath:str):
 
     return entries
 
-def get_unique_log_levels(logs:list[tuple[str, str, str, str]]):
+def get_unique_log_levels(logs:list[tuple[str, str, str]]):
     """Return set of all log levels found"""
     log_levels_set:set[str] = set()
 
     for log in logs:
-      log_levels_set.add(log[2])
+      log_levels_set.add(log[1])
 
     return log_levels_set
 
 
-def filter_by_level(logs:list[tuple[str, str, str, str]], level:str):
+def filter_by_level(logs:list[tuple[str, str, str]], level:str):
     """Return list of logs with specific level"""
     level = level.upper()
-    specific_level_logs:list[tuple[str, str, str, str]] = []
+    specific_level_logs:list[tuple[str, str, str]] = []
     for log in logs:
       if level in log:
         specific_level_logs.append(log)
 
     return specific_level_logs
 
-def extract_errors(logs):
+def extract_errors(logs:list[tuple[str, str, str]]):
     """Return list of error tuples"""
-    pass
+    return filter_by_level(logs,"error")
 
-def count_logs_by_level(logs):
+def count_logs_by_level(logs:list[tuple[str, str, str]]):
     """Return dict: {level: count}"""
-    pass
+    logs_count: dict[str,int] = {}
 
-def get_logs_after_time(logs, timestamp):
+    for log in logs:
+      level = log[1]
+      if level in logs_count:
+        logs_count[level] += 1
+      else:
+        logs_count[level] = 1
+
+    return logs_count
+
+def get_logs_after_time(logs:list[tuple[str, str, str]], timestamp:str):
     """Return logs after given timestamp"""
-    pass
+    if validate_date_time(timestamp) is False:
+      print("Invalid Timestamp")
+      return
+
+    filtered_logs:list[tuple[str, str, str]] = []
+
+    for log in logs:
+      log_timestamp = log[0]
+
+      if is_after_timestamp(log_timestamp,timestamp) is True:
+        filtered_logs.append(log)
+
+    return filtered_logs
 
 
-# print(parse_log_file("./sample.log"))
-parsed_logs = parse_log_file("./sample.log")
-print("GET UNIQUE LOG LEVELS: \n",get_unique_log_levels(parsed_logs),"\n")
-print("FILTER BY LEVLE: \n",filter_by_level(parsed_logs,"error"),"\n")
+def main():
+  parsed_logs = parse_log_file("./sample.log")
+  reference_timestamp = "2024-01-15 10:20:00"
+
+  if parsed_logs is None or not parsed_logs :
+    print("File dosen't exists or it's empty")
+    return
+
+  print("GET UNIQUE LOG LEVELS: \n",get_unique_log_levels(parsed_logs),"\n")
+  print("FILTER BY LEVLE: \n",filter_by_level(parsed_logs,"error"),"\n")
+  print("COUNT LOGS BY LEVLE: \n",count_logs_by_level(parsed_logs),"\n")
+  print("GET LOGS AFTER TIME: \n",get_logs_after_time(parsed_logs,reference_timestamp),"\n")
+
+
+main()
--- utils.py
+++ utils.py
@@ -1,34 +1,42 @@
 from datetime import datetime
 
-log_levels = ("ERROR","INFO","WARNING")
+LOG_LEVELS = ("ERROR","INFO","WARNING")
+DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 def parse_log_lines(log:str):
-  """Return tuple of log (day,time,level,message)"""
-  splited_log = log.split(" ")
+  """Return tuple of log (day,level,message)"""
+  splited_log = log.split(" ",3)
+
+  if len(splited_log) < 3:
+    return
+
   date = splited_log[0]
   time = splited_log[1]
 
   if validate_date_time(date + " " + time) is False:
-    # print("Invalid date & Time")
     return
 
   level = splited_log[2]
 
   if validate_log_levels(level) is False:
-    # print("Invalid level")
     return
 
   message = splited_log[3:]
 
-  return (date,time,level," ".join(str(x) for x in message))
+  return (date + " " + time,level," ".join(str(x) for x in message))
 
 def validate_date_time(date_time:str):
   try:
-    format = "%Y-%m-%d %H:%M:%S"
-    _ = datetime.strptime(date_time,format).astimezone()
+    _ = datetime.strptime(date_time,DATETIME_FORMAT).astimezone()
     return True
   except ValueError:
     return False
 
 def validate_log_levels(log:str):
-  return log in log_levels
+  return log in LOG_LEVELS
+
+def is_after_timestamp(log_timestamp:str,reference_timestamp:str):
+  log_time = datetime.strptime(log_timestamp,DATETIME_FORMAT).astimezone()
+  reference_time = datetime.strptime(reference_timestamp,DATETIME_FORMAT).astimezone()
+
+  return log_time > reference_time
